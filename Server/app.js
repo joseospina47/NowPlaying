02684c68d15a34cc0f9d3da8e04d3ef28e8dcfc2
Jose Ospina <jose.ospina.hurtado@gmail.com>
2016-10-31 16:13:21 -0500

    Server: Cors enabled

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,5 @@
 const app         = require('express')();
+const cors              = require('cors')
 const bodyParser  = require('body-parser');
 const routes      = require('./app/app.routes');
 
@@ -9,6 +10,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api', routes);
 
 app.listen(8001, () => {
